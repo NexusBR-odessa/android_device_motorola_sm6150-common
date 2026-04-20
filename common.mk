8b0a7671b66e85d8c0514a4e0b095306541a2dca
miguelbarretoo <miguel03barreto@gmail.com>
2026-04-20 03:35:03 +0000

    sm6150-common: miguel baitola

--- common.mk
+++ common.mk
@@ -177,8 +177,12 @@ PRODUCT_COPY_FILES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot-service.qti \
-    android.hardware.boot-service.qti.recovery
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-service \
+    android.hardware.boot@1.1-impl-qti.recovery
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
 
 $(call soong_config_set_bool,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
 
